6c74ea2274722dbb5b42db5582d12813e83f5b78
Jan Gieseler <jan.gieseler@utu.fi>
2025-03-25 11:53:22 +0200

    code cleaning

--- fluence/tests/test_.py
+++ fluence/tests/test_.py
@@ -44,19 +44,14 @@ def test_SEP_Fluence_Spectra():
     # set your local path where you want to save the data files:
     data_path = f"{os.getcwd()}/data/"
     #
-    print('init')
     E = Event()
-    print('load')
     E.load_data(w.spacecraft_drop.value, w.sensor_drop.value, w.species_drop.value, startdate, enddate, w.view_drop.value, resample, data_path)
     #
-    print('plot 1')
     fig, ax = E.plot_flux(integration_start, integration_end, subtract_background=subtract_background,
                           background_start=background_start, background_end=background_end, savefig=False)
     #
-    print('spec')
     E.get_integrated_spec(integration_start, integration_end, subtract_background=subtract_background,
                           background_start=background_start, background_end=background_end)
     #
-    print('plot 2')
     fig, ax = E.plot_spectrum(savefig=False)
     return fig
