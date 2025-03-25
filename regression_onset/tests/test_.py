85d022e3716801ba1df004cc6aa524ab75cdc093
Jan Gieseler <jan.gieseler@utu.fi>
2025-03-25 16:49:58 +0200

    update regression test

--- regression_onset/tests/test_.py
+++ regression_onset/tests/test_.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import os
 import pandas as pd
 from seppy.tools import Event
@@ -28,18 +29,16 @@ def test_SEP_Regression_Analysis():
     #
     # This is the path to your data directory
     path = f"{os.getcwd()}{os.sep}data"
-    #
-    # The name of your data file, if you're loading in your own data.
+    # The name of your data file, if you're loading in your own data. 
     filename = "solo_ept_sun_e.csv"
-    #
     # To download (or load if files are locally present) SEPpy data, one needs to provide a time span.
     # If you're not using SEPpy, this can be ignored.
-    start_date = "2022-01-20"
-    end_date = "2022-01-21"
-    #
+    # The format is (year, month, day)
+    start_date = dt.datetime(2022, 1, 20)
+    end_date = dt.datetime(2022, 1, 21)
     if select_data._seppy_selected(select_data.data_file):
         import seppy.tools.widgets as w
-        display(w.spacecraft_drop, w.sensor_drop, w.view_drop, w.species_drop)
+        display(w.spacecraft_drop, w.sensor_drop, w.view_drop, w.species_drop)    
     #
     # manual select SEPpy selection:
     # w.spacecraft_drop.value = 'STEREO-A'
@@ -50,8 +49,8 @@ def test_SEP_Regression_Analysis():
     if select_data._seppy_selected(select_data.data_file):
         # Initializes the SEPpy Event object
         seppy_data = Event(spacecraft=w.spacecraft_drop.value, sensor=w.sensor_drop.value, species=w.species_drop.value,
-                           start_date=start_date, end_date=end_date, data_level="l2",
-                           data_path=path, viewing=w.view_drop.value)
+                            start_date=start_date, end_date=end_date, data_level="l2",
+                            data_path=path, viewing=w.view_drop.value)
         # Exports the data to a pandas dataframe
         df = reg.externals.export_seppy_data(event=seppy_data)
     else:
@@ -62,40 +61,37 @@ def test_SEP_Regression_Analysis():
     #
     # Initializing the tool with input data
     event = reg.Reg(data=df)
-    #
-    # Choose the channel(s) to examine in quicklook plot
+    # Choose the channel (column name, see display(df) above)
     channel = "E4"
-    #
-    # Leaving selection to None
+    # see explanation above for details 
+    # selection = ["2022-01-20 02:00", "2022-01-20 12:00"]
     selection = None
-    selection = ["2022-01-20 02:00", "2022-01-20 12:00"]
-    #
     # Display a quicklook plot of the input data (df).
     # Apply the selection of data for the tool by 'selection' parameter or by clicking
     # on the plot.
     # The line magic 'ipympl' enables interactive mode
     # %matplotlib ipympl
-    event.quicklook(channel=channel, resample="5 min", selection=selection)
-    #
-    # Title for the figure (optional)
-    title = r"Solar Orbiter / EPT$^{\mathrm{sun}}$ ($0.0439 - 0.0467$) MeV electrons, 1 min data"
-    #
-    # The channel to consider
-    # channel = "E5"
-    #
-    # Time-averages the data to given cadence
-    resample = "1 min"
+    event.quicklook(channel=channel, resample="5min", selection=selection)  
     #
     # The number of breakpoints to seek from the data selection
     num_of_breaks = 3
     #
-    # Boundaries of the time axis
-    xlim = ["2022-01-20 00:00", "2022-01-21 06:00"]
-    #
     # Fills zero counts with a filler falue f
     fill_zeroes = True
     #
+    #
+    # Time-averages the data to given cadence
+    resample = "1min"
+    #
+    # Boundaries of the time axis
+    xlim = ["2022-01-20 00:00", "2022-01-21 06:00"]
+    #
+    # Title for the figure (optional)
+    if select_data._seppy_selected(select_data.data_file):
+        title = r"Solar Orbiter / EPT$^{\mathrm{sun}}$ ($0.0439 - 0.0467$) MeV electrons, 1 min data"
+    else:
+        title = ''  # set own title for User defined file 
+    #
     # %matplotlib inline
-    results = event.find_breakpoints(channel=channel, breaks=num_of_breaks, fill_zeroes=fill_zeroes,
-                                     xlim=xlim, title=title, resample=resample)
+    results = event.find_breakpoints(channel=channel, breaks=num_of_breaks, fill_zeroes=fill_zeroes, xlim=xlim, title=title, resample=resample)
     return results['fig']
