111bc8dd936e687d6f44f1957a834946c7baa029
Jan Gieseler <jan.gieseler@utu.fi>
2025-11-14 16:37:08 +0200

    update baseline images and update test cases for SEP_Spectra functionality

--- spectra/tests/test_.py
+++ spectra/tests/test_.py
@@ -25,19 +25,19 @@ pytest -ra --mpl --mpl-baseline-path=baseline --mpl-baseline-relative --mpl-gene
 """
 
 
-@pytest.mark.parametrize("spectral_type", ['integral', 'peak'])
+@pytest.mark.parametrize("spectral_type, species", [('integral', 'protons'), ('peak', 'protons')])
 @pytest.mark.mpl_image_compare(remove_text=False, deterministic=True)
 @pytest.mark.filterwarnings("ignore:FigureCanvasAgg is non-interactive, and thus cannot be shown:UserWarning")
 @pytest.mark.filterwarnings("ignore::UserWarning:seppy")
 @pytest.mark.filterwarnings("ignore::UserWarning:sunpy")
 @pytest.mark.filterwarnings("ignore:Mean of empty slice:RuntimeWarning")
-def test_SEP_Spectra_PSP_ISOIS_EPIHI(spectral_type):
+def test_SEP_Spectra_PSP_ISOIS_EPIHI(spectral_type, species):
     display(w.spacecraft_drop, w.sensor_drop, w.view_drop, w.species_drop)
     #
     w.spacecraft_drop.value = 'PSP'
     w.sensor_drop.value = 'isois-epihi'
     w.view_drop.value = 'A'
-    w.species_drop.value = 'protons'
+    w.species_drop.value = species
     #
     # spectral integration interval:
     startdate = dt.datetime(2021, 10, 28)
@@ -71,13 +71,58 @@ def test_SEP_Spectra_PSP_ISOIS_EPIHI(spectral_type):
     return fig
 
 
-@pytest.mark.parametrize("spectral_type", ['integral', 'peak'])
+@pytest.mark.parametrize("spectral_type, species", [('peak', 'protons')])
 @pytest.mark.mpl_image_compare(remove_text=False, deterministic=True)
 @pytest.mark.filterwarnings("ignore:FigureCanvasAgg is non-interactive, and thus cannot be shown:UserWarning")
 @pytest.mark.filterwarnings("ignore::UserWarning:seppy")
 @pytest.mark.filterwarnings("ignore::UserWarning:sunpy")
 @pytest.mark.filterwarnings("ignore:Mean of empty slice:RuntimeWarning")
-def test_SEP_Spectra_SOHO_ERNE_HED(spectral_type):
+def test_SEP_Spectra_SOHO_ERNE_HED(spectral_type, species):
+    display(w.spacecraft_drop, w.sensor_drop, w.view_drop, w.species_drop)
+    #
+    w.spacecraft_drop.value = 'SOHO'
+    w.sensor_drop.value = 'ERNE-HED'
+    # w.view_drop.value = 'asun'
+    w.species_drop.value = species
+    #
+    # spectral integration interval:
+    startdate = dt.datetime(2021, 10, 28)
+    enddate = dt.datetime(2021, 11, 2)
+    #
+    subtract_background = True
+    background_start = dt.datetime(2021, 10, 28, 2, 0)
+    background_end = dt.datetime(2021, 10, 28, 14, 0)
+    #
+    integration_start = dt.datetime(2021, 10, 28, 16, 0)
+    integration_end = dt.datetime(2021, 11, 1)
+    #
+    resample = '30min'  # '60s'
+    #
+    # set your local path where you want to save the data files:
+    data_path = f"{os.getcwd()}{os.sep}data"
+    data_path = jupyterhub_data_path(data_path)
+    #
+    E = Event()
+    E.load_data(w.spacecraft_drop.value, w.sensor_drop.value, w.species_drop.value, startdate, enddate, w.view_drop.value, resample, data_path)
+    #
+    fig, ax = E.plot_flux(integration_start, integration_end, subtract_background=subtract_background,
+                          background_start=background_start, background_end=background_end,
+                          savefig=False, spec_type=spectral_type)
+    #
+    E.get_spec(integration_start, integration_end, subtract_background=subtract_background,
+                          background_start=background_start, background_end=background_end,
+                          spec_type=spectral_type)
+    #
+    fig, ax = E.plot_spectrum(savefig=False)
+    return fig
+
+
+@pytest.mark.mpl_image_compare(remove_text=False, deterministic=True)
+@pytest.mark.filterwarnings("ignore:FigureCanvasAgg is non-interactive, and thus cannot be shown:UserWarning")
+@pytest.mark.filterwarnings("ignore::UserWarning:seppy")
+@pytest.mark.filterwarnings("ignore::UserWarning:sunpy")
+@pytest.mark.filterwarnings("ignore:Mean of empty slice:RuntimeWarning")
+def test_SEP_Spectra_SOHO_ERNE_HED_None():
     display(w.spacecraft_drop, w.sensor_drop, w.view_drop, w.species_drop)
     #
     w.spacecraft_drop.value = 'SOHO'
@@ -107,30 +152,29 @@ def test_SEP_Spectra_SOHO_ERNE_HED(spectral_type):
     #
     fig, ax = E.plot_flux(integration_start, integration_end, subtract_background=subtract_background,
                           background_start=background_start, background_end=background_end,
-                          savefig=False, spec_type=spectral_type)
+                          savefig=False)
     #
     E.get_spec(integration_start, integration_end, subtract_background=subtract_background,
-                          background_start=background_start, background_end=background_end,
-                          spec_type=spectral_type)
+                          background_start=background_start, background_end=background_end)
     #
     fig, ax = E.plot_spectrum(savefig=False)
     return fig
 
 
-@pytest.mark.parametrize("spectral_type", ['integral', 'peak'])
+@pytest.mark.parametrize("spectral_type, species", [('integral', 'electrons'), ('peak', 'ions')])
 @pytest.mark.mpl_image_compare(remove_text=False, deterministic=True)
 @pytest.mark.filterwarnings("ignore:FigureCanvasAgg is non-interactive, and thus cannot be shown:UserWarning")
 @pytest.mark.filterwarnings("ignore::UserWarning:seppy")
 @pytest.mark.filterwarnings("ignore::UserWarning:solo_epd_loader")
 @pytest.mark.filterwarnings("ignore::UserWarning:sunpy")
 @pytest.mark.filterwarnings("ignore:Mean of empty slice:RuntimeWarning")
-def test_SEP_Spectra_Solar_Orbiter_EPT(spectral_type):
+def test_SEP_Spectra_Solar_Orbiter_EPT(spectral_type, species):
     display(w.spacecraft_drop, w.sensor_drop, w.view_drop, w.species_drop)
     #
     w.spacecraft_drop.value = 'Solar Orbiter'
     w.sensor_drop.value = 'EPT'
     w.view_drop.value = 'asun'
-    w.species_drop.value = 'ions'
+    w.species_drop.value = species
     #
     # spectral integration interval:
     startdate = dt.datetime(2021, 10, 28)
@@ -164,20 +208,20 @@ def test_SEP_Spectra_Solar_Orbiter_EPT(spectral_type):
     return fig
 
 
-@pytest.mark.parametrize("spectral_type", ['integral', 'peak'])
+@pytest.mark.parametrize("spectral_type, species", [('integral', 'electrons'), ('peak', 'protons')])
 @pytest.mark.mpl_image_compare(remove_text=False, deterministic=True)
 @pytest.mark.filterwarnings("ignore:FigureCanvasAgg is non-interactive, and thus cannot be shown:UserWarning")
 @pytest.mark.filterwarnings("ignore::UserWarning:seppy")
 @pytest.mark.filterwarnings("ignore::UserWarning:solo_epd_loader")
 @pytest.mark.filterwarnings("ignore::UserWarning:sunpy")
 @pytest.mark.filterwarnings("ignore:Mean of empty slice:RuntimeWarning")
-def test_SEP_Spectra_Solar_Orbiter_HET(spectral_type):
+def test_SEP_Spectra_Solar_Orbiter_HET(spectral_type, species):
     display(w.spacecraft_drop, w.sensor_drop, w.view_drop, w.species_drop)
     #
     w.spacecraft_drop.value = 'Solar Orbiter'
     w.sensor_drop.value = 'HET'
     w.view_drop.value = 'asun'
-    w.species_drop.value = 'electrons'
+    w.species_drop.value = species
     #
     # spectral integration interval:
     startdate = dt.datetime(2021, 10, 28)
@@ -211,19 +255,19 @@ def test_SEP_Spectra_Solar_Orbiter_HET(spectral_type):
     return fig
 
 
-@pytest.mark.parametrize("spectral_type", ['integral', 'peak'])
+@pytest.mark.parametrize("spectral_type, species", [('integral', 'electrons'), ('peak', 'ions')])
 @pytest.mark.mpl_image_compare(remove_text=False, deterministic=True)
 @pytest.mark.filterwarnings("ignore:FigureCanvasAgg is non-interactive, and thus cannot be shown:UserWarning")
 @pytest.mark.filterwarnings("ignore::UserWarning:seppy")
 @pytest.mark.filterwarnings("ignore::UserWarning:sunpy")
 @pytest.mark.filterwarnings("ignore:Mean of empty slice:RuntimeWarning")
-def test_SEP_Spectra_STEREO_A_SEPT(spectral_type):
+def test_SEP_Spectra_STEREO_A_SEPT(spectral_type, species):
     display(w.spacecraft_drop, w.sensor_drop, w.view_drop, w.species_drop)
     #
     w.spacecraft_drop.value = 'STEREO-A'
     w.sensor_drop.value = 'SEPT'
     w.view_drop.value = 'asun'
-    w.species_drop.value = 'electrons'
+    w.species_drop.value = species
     #
     # spectral integration interval:
     startdate = dt.datetime(2021, 10, 28)
@@ -257,19 +301,19 @@ def test_SEP_Spectra_STEREO_A_SEPT(spectral_type):
     return fig
 
 
-@pytest.mark.parametrize("spectral_type", ['peak'])
+@pytest.mark.parametrize("spectral_type, species", [('integral', 'electrons'), ('peak', 'protons')])
 @pytest.mark.mpl_image_compare(remove_text=False, deterministic=True)
 @pytest.mark.filterwarnings("ignore:FigureCanvasAgg is non-interactive, and thus cannot be shown:UserWarning")
 @pytest.mark.filterwarnings("ignore::UserWarning:seppy")
 @pytest.mark.filterwarnings("ignore::UserWarning:sunpy")
 @pytest.mark.filterwarnings("ignore:Mean of empty slice:RuntimeWarning")
-def test_SEP_Spectra_STEREO_A_HET(spectral_type):
+def test_SEP_Spectra_STEREO_A_HET(spectral_type, species):
     display(w.spacecraft_drop, w.sensor_drop, w.view_drop, w.species_drop)
     #
     w.spacecraft_drop.value = 'STEREO-A'
     w.sensor_drop.value = 'HET'
     # w.view_drop.value = 'asun'
-    w.species_drop.value = 'electrons'
+    w.species_drop.value = species
     #
     # spectral integration interval:
     startdate = dt.datetime(2021, 10, 28)
@@ -303,18 +347,19 @@ def test_SEP_Spectra_STEREO_A_HET(spectral_type):
     return fig
 
 
+@pytest.mark.parametrize("spectral_type, species", [('integral', 'electrons'), ('peak', 'protons')])
 @pytest.mark.mpl_image_compare(remove_text=False, deterministic=True)
 @pytest.mark.filterwarnings("ignore:FigureCanvasAgg is non-interactive, and thus cannot be shown:UserWarning")
 @pytest.mark.filterwarnings("ignore::UserWarning:seppy")
 @pytest.mark.filterwarnings("ignore::UserWarning:sunpy")
 @pytest.mark.filterwarnings("ignore:Mean of empty slice:RuntimeWarning")
-def test_SEP_Spectra_STEREO_A_HET_None():
+def test_SEP_Spectra_Wind_3DP(spectral_type, species):
     display(w.spacecraft_drop, w.sensor_drop, w.view_drop, w.species_drop)
     #
-    w.spacecraft_drop.value = 'STEREO-A'
-    w.sensor_drop.value = 'HET'
-    # w.view_drop.value = 'asun'
-    w.species_drop.value = 'electrons'
+    w.spacecraft_drop.value = 'Wind'
+    w.sensor_drop.value = '3DP'
+    w.view_drop.value = 'omnidirectional'
+    w.species_drop.value = species
     #
     # spectral integration interval:
     startdate = dt.datetime(2021, 10, 28)
@@ -338,10 +383,11 @@ def test_SEP_Spectra_STEREO_A_HET_None():
     #
     fig, ax = E.plot_flux(integration_start, integration_end, subtract_background=subtract_background,
                           background_start=background_start, background_end=background_end,
-                          savefig=False)
+                          savefig=False, spec_type=spectral_type)
     #
     E.get_spec(integration_start, integration_end, subtract_background=subtract_background,
-                          background_start=background_start, background_end=background_end)
+                          background_start=background_start, background_end=background_end,
+                          spec_type=spectral_type)
     #
     fig, ax = E.plot_spectrum(savefig=False)
     return fig
