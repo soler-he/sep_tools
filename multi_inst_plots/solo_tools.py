fix: update ion temperature calculation to force-use SI units for e

multi_inst_plots/solo_tools.py
@@ -263,7 +263,7 @@ def load_data(options):
             swa_vsw = np.sqrt(swa_data.V_RTN_0**2 + swa_data.V_RTN_1**2 + swa_data.V_RTN_2**2)
             swa_data['vsw'] = swa_vsw
 
-            temp = np.sqrt(swa_data.TxTyTz_RTN_0**2 + swa_data.TxTyTz_RTN_1**2 + swa_data.TxTyTz_RTN_2**2) * e / k_B 
+            temp = np.sqrt(swa_data.TxTyTz_RTN_0**2 + swa_data.TxTyTz_RTN_1**2 + swa_data.TxTyTz_RTN_2**2) * e.si / k_B
             swa_data['temp'] = temp
             swa_data['p_dyn'] = m_p * swa_data['N'] * 1e6 * (swa_data['vsw'] * 1e3)**2 * 1e9    # nPa
 